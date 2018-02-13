fa2e1b6000761625a39ce3eebe7c3dc115aa685b
siyaoxu <siyaoxu@alumni.stanford.edu>
2018-02-12 23:56:38 -0600

    updates on MPC project v-1

---
+++ CarND-MPC-Project/cmake-build-debug/CMakeFiles/mpc.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpc.dir/src/MPC.cpp.obj"
+  "CMakeFiles/mpc.dir/src/main.cpp.obj"
+  "mpc.pdb"
+  "mpc.exe"
+  "mpc.exe.manifest"
+  "libmpc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
